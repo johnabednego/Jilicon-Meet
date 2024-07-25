b026fa0e831e91461ee8724d5e2c19d143fd4620
johnabednego <jilimajohnabednego@gmail.com>
2024-07-25 03:11:13 +0000

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jilicon Meet",
-  description: "A video calling app built with Next.js & Stream",
+  description: "A fully-fledged video calling application built with Stream's React Video SDK and Next.js, featuring mic & cam controls, roles & permissions, call recordings, screen sharing, picture-in-picture, and more.",
+  keywords: "video calling, Stream React Video SDK, Next.js, mic controls, cam controls, roles, permissions, call recordings, screen sharing, picture-in-picture",
+  authors: [{ name: "John Abednego Jilima", url: "https://abednegoportfolio.netlify.app/" }],
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow",
+  openGraph: {
+    title: "Jilicon Meet",
+    description: "Experience seamless video calling with Jilicon Meet, built with Stream's React Video SDK and Next.js. Enjoy features like mic & cam controls, roles & permissions, call recordings, screen sharing, picture-in-picture, and more.",
+    url: "https://jilicon-meet.vercel.app/",
+    type: "website",
+  },
 };
+
+
 
 export default function RootLayout({
   children,
